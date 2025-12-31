039c94146b3cc2a4e88f67969e38876700efb58a
Gary Waterworth <gdwaterworth@gmail.com>
2025-12-31 23:15:22 +0200

    .

--- src/clients/mqtt_client.py
+++ src/clients/mqtt_client.py
@@ -7,13 +7,35 @@ from src.configuration.configuration import Configuration
 # "MQTT_Port": 0,
 # "MQTT_Login": "",
 # "MQTT_Pass": ""
-
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
 topic = "python/mqtt"
+
 # Generate a Client ID with the publish prefix.
 client_id = f'publish-{random.randint(0, 1000)}'
 
 class MqttClient:
+
     def connect_mqtt():
+        def on_disconnect(client, userdata, rc):
+            print(f"Disconnected with result code: `{rc}`")
+            reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+            while reconnect_count < MAX_RECONNECT_COUNT:
+                print(f"Reconnecting in `{reconnect_delay}` seconds...")
+                time.sleep(reconnect_delay)
+                try:
+                    client.reconnect()
+                    print(f"Reconnected successfully!")
+                    return
+                except Exception as err:
+                    print(f"`{err}`. Reconnect failed. Retrying...")
+
+                reconnect_delay *= RECONNECT_RATE
+                reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+                reconnect_count += 1
+            print(f"Reconnect failed after `{reconnect_delay}` attempts. Exiting...")
         def on_connect(client, userdata, flags, rc):
             if rc == 0:
                 print("Connected to MQTT Broker!")
@@ -33,10 +55,10 @@ class MqttClient:
         client.username_pw_set(username, password)
         client.on_connect = on_connect
         client.connect(broker, port)
+        client.on_disconnect = on_disconnect    
         return client
-
-    def publish(client):
-            msg = f"messages: 1"
+        
+    def publish(client,topic,msg):
             result = client.publish(topic, msg)
             status = result[0]
             if status == 0:
